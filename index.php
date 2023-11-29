9d805404444e3397e914500fedbbdffc090c3454
Michael Lewis <mjlewis@cims.nyu.edu>
2023-11-28 21:02:31 -0500

    test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<?PHP include "head.php"; include "track.php"; ?>
+<body>
+<div id="title"><h1>The Den of Inequality</h1></div>
+<div id="container">
+<?PHP include "menu.php"; ?>
+
+<div id="main">
+<div style="float: right"><img border = "1" height= 360 px, src = "mypic4.jpg" ></div>
+<font size = 4 ><strong>Michael Lewis</strong></font><br>
+Mathematics Ph.D. candidate <br>
+Courant Institute of Mathematical Sciences<br>
+<table border="0">
+<tr> <td><i>Advisor</i></td><td><a href="http://math.nyu.edu/faculty/goodman/">Jonathan Goodman</a></td> </tr>
+<tr> <td><i>Fields of Interest</i></td><td>Monte Carlo</td></tr>
+<tr> <td> </td> <td>Numerical Methods</td> </tr>
+<tr> <td> </td> <td>Probability</td> </tr>
+</table>
+</div>
+<?PHP include "credits.php"; ?>
+<div id="footer"></div>
+</div>
+</body>
+</html>
