62be3950f6ccabfe57af832b1bd58f4d79dd9562
Michael Lewis <mjlewis@cims.nyu.edu>
2023-11-28 21:07:59 -0500

    test2

---
+++ credits.php
@@ -0,0 +1,32 @@
+<br clear="left">
+<p class="credits" align="left">&copy;
+2012 
+<SCRIPT LANGUAGE="javascript"> 
+ 
+    var user = location.href.replace(/.*\~/,'').replace(/\/.*/,'') ; 
+ 
+    var first = 'ma';
+    var second = 'il';
+    var third = 'to:';
+ 
+    var address = user;
+    var domain = 'cims.nyu';
+    var ext = 'edu';
+    document.write('<a href="');
+    document.write(first+second+third);
+    document.write(address);
+    document.write('&#64;');
+    document.write(domain);
+    document.write('.');
+    document.write(ext);  
+    document.write('" style="color: gray;'); 
+    document.write('">'); 
+ 
+    document.write('Michael Lewis');
+ 
+    document.write('</a>');
+</script>
+<noscript>Michael Lewis</noscript> 
+| Design by <a href="http://andreasviklund.com">Andreas Viklund</a><br>
+<?php system('perl foo.pl'); ?>
+</p>
